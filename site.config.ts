47b78bc483e2888615cf56366cff9cbf11e0d792
Indeavr <donevskimartin@gmail.com>
2025-03-29 10:39:19 +0200

    siteconfig

--- site.config.ts
+++ site.config.ts
@@ -16,9 +16,9 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'indeavr',
-  github: 'indeavr',
-  linkedin: 'donevskimartin',
+  // twitter: 'indeavr',
+  // github: 'indeavr',
+  // linkedin: 'donevskimartin',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
