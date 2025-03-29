1359c15c71cbef99a82ad08883b815ec504da6d0
Indeavr <donevskimartin@gmail.com>
2025-03-29 13:23:28 +0200

    speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,7 @@ import 'styles/global.css'
 import 'styles/notion.css'
 // global style overrides for prism theme (optional)
 import 'styles/prism-theme.css'
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { AppProps } from 'next/app'
 import * as Fathom from 'fathom-client'
 import { useRouter } from 'next/router'
